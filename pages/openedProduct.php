cf682c235bc4e571c2c092f353548d69a280700d
Katerisa <kate.sam@mail.ru>
2019-09-24 21:29:38 +0500

    first commit

---
+++ pages/openedProduct.php
@@ -0,0 +1,14 @@
+<div class='openedProduct'>
+    <div class='openedProductImg'>
+        <img src='<?php echo $good['img']; ?>' >
+    </div>
+    <div class='openedProductContent'>
+    <h1 class='openedProductName'><?php echo $good['name']; ?></h1>
+    <div class='openedProductDesc'>
+        <?php echo $good['desc']; ?>
+    </div>
+    <div class='openedProductPrice'>
+        Цена: <?php echo $good['price']; ?>
+    </div>
+</div>
+</div>
---
+++ pages/shop.php
@@ -0,0 +1,23 @@
+<div class="shopContent">
+    <h1>Каталог товаров</h1>
+    <div class='shopItems'>
+        <?php foreach ($goods as $good) : ?>
+        <div class='shopUnit'>
+            <img src='<?php echo $good['img']; ?>' >
+            <div class='shopUnitName'>
+                <?php echo $good['name']; ?>
+            </div>
+            <div class='shopUnitShortDesc'>
+                <?php echo $good['desc']; ?>
+            </div>
+            <div class='shopUnitPrice'>
+                Цена: <?php echo $good['price']; ?>
+            </div>
+            <a href='index.php?page=product&id=<?php echo $good['id'] ?>' class='shopUnitMore'>
+                Подробнее
+            </a>
+        </div>
+        <?php endforeach; ?>
+    </div>
+    
+</div>
